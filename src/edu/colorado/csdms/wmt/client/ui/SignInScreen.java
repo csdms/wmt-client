b962d32c3eeae07765a3cc487e4c5e73eca7ff0a
Mark Piper <mark.piper@colorado.edu>
2014-07-15 10:18:15 -0600

    Add handlers for question links to sign-in screen
    The "New User?" and "Forgot Password?" questions on the sign-in screen
    now have ClickHandlers that display information.
    
    Also changed vertical alignment of content: instead of center-aligned,
    it's now top-aligned with an offset calculated from the browser window
    height.
    
    The width of the content is also constrained by the browser window
    width.

--- src/edu/colorado/csdms/wmt/client/ui/SignInScreen.java
+++ src/edu/colorado/csdms/wmt/client/ui/SignInScreen.java
@@ -1,6 +1,10 @@
 package edu.colorado.csdms.wmt.client.ui;
 
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Cookies;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -32,19 +36,29 @@ public class SignInScreen extends HorizontalPanel {
    */
   public SignInScreen() {
 
-    this.setStyleName("wmt-SignInScreen");
+    // Determine offset from top of browser window.
+    Integer browserHeight = Window.getClientHeight();
+    Double offset = 0.20; // arbitrary, aesthetic
+    Double contentOffset = offset*browserHeight;
+
+    Integer browserWidth = Window.getClientWidth();
+    Double widthFraction = 0.40; // arbitrary, aesthetic
+    Integer contentWidth = (int) Math.round(widthFraction*browserWidth);
     
-    // Alignments that apply to all widgets hereafter.
-    this.setHorizontalAlignment(ALIGN_CENTER); 
-    this.setVerticalAlignment(ALIGN_MIDDLE);
+    // Styles and alignments that apply to all widgets hereafter.
+    this.setStyleName("wmt-SignInScreen");
+    this.setHorizontalAlignment(ALIGN_CENTER);
     
     // Organizer for the contents of the SignInScreen.
     contents = new VerticalPanel();
     contents.setHorizontalAlignment(ALIGN_CENTER);
+    contents.setWidth(contentWidth.toString() + "px");
     this.add(contents);
     
+    // The title's margin is used to offset content from the top of the window.
     Label title = new Label("The CSDMS Web Modeling Tool");
     title.setStyleName("wmt-SignInScreenTitle");
+    title.getElement().getStyle().setMarginTop(contentOffset, Unit.PX);
     
     // Use a Cookie and a SuggestBox to help autocomplete the user's login.
     // TODO Replace with the browser's login autocomplete mechanism.
@@ -69,12 +83,35 @@ public class SignInScreen extends HorizontalPanel {
     // Question links.
     HTML newUser = new HTML("<i class='fa fa-arrow-right'></i> New User?");
     newUser.setStyleName("wmt-SignInScreenLinks");
+    String newUserText = "Create a new sign-in to WMT with a preferred email"
+        + " address and password. To authenticate your sign-in, you'll be"
+        + " asked to repeat your password.";
+    HTML newUserInfo = new HTML(newUserText);
+    newUserInfo.setStyleName("wmt-SignInScreenLinksInfo");
+    final HorizontalPanel newUserInfoPanel = new HorizontalPanel();
+    newUserInfoPanel.setStyleName("wmt-SignInScreenLinksInfoPanel");
+    newUserInfoPanel.add(newUserInfo);
+    newUserInfoPanel.setVisible(false);
+    
     HTML forgotPassword = new HTML("<i class='fa fa-arrow-right'></i> Forgot Password?");    
     forgotPassword.setStyleName("wmt-SignInScreenLinks");
+    String forgotPasswordText = "Please contact CSDMS Support"
+        + " (CSDMSSupport@colorado.edu) for assistance.";
+    HTML forgotPasswordInfo = new HTML(forgotPasswordText);
+    forgotPasswordInfo.setStyleName("wmt-SignInScreenLinksInfo");
+    final HorizontalPanel forgotPasswordInfoPanel = new HorizontalPanel();
+    forgotPasswordInfoPanel.setStyleName("wmt-SignInScreenLinksInfoPanel");
+    forgotPasswordInfoPanel.add(forgotPasswordInfo);
+    forgotPasswordInfoPanel.setVisible(false);
+    
+    
     VerticalPanel linksPanel = new VerticalPanel();
+    linksPanel.setHorizontalAlignment(ALIGN_CENTER);
     linksPanel.setStyleName("wmt-SignInScreenLinksPanel");
     linksPanel.add(newUser);
+    linksPanel.add(newUserInfoPanel);
     linksPanel.add(forgotPassword);
+    linksPanel.add(forgotPasswordInfoPanel);
     
     // Load the contents of the SignInScreen.
     contents.add(title);
@@ -82,5 +119,19 @@ public class SignInScreen extends HorizontalPanel {
     contents.add(passwordBox);
     contents.add(signInButton);
     contents.add(linksPanel);
+    
+    newUser.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        newUserInfoPanel.setVisible(!newUserInfoPanel.isVisible());
+      }
+    });
+    
+    forgotPassword.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent event) {
+        forgotPasswordInfoPanel.setVisible(!forgotPasswordInfoPanel.isVisible());
+      }
+    });    
   }
 }
