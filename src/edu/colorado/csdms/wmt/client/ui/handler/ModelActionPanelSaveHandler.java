11faa3b14c1bc3cffa5d3d8602b214552d995930
Mark Piper <mark.piper@colorado.edu>
2015-07-09 12:15:05 -0600

    Check for model ownership before Save or Save As action
    This is related to the problem raised in #55, but in that case, the
    user has modified the model, so the internal modelIsSaved flag is
    false. In this case, the user doesn't modify the model, but selects
    "More > Save Model As...". This commit addresses both cases.
    
    This commit also fixes #67.

--- src/edu/colorado/csdms/wmt/client/ui/handler/ModelActionPanelSaveHandler.java
+++ src/edu/colorado/csdms/wmt/client/ui/handler/ModelActionPanelSaveHandler.java
@@ -1,26 +1,3 @@
-/**
- * The MIT License (MIT)
- * 
- * Copyright (c) 2014 mcflugen
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 package edu.colorado.csdms.wmt.client.ui.handler;
 
 import java.util.Map;
@@ -67,35 +44,32 @@ public class ModelActionPanelSaveHandler implements ClickHandler {
         showSaveDialogBox(Constants.MODELS_NEW_PATH);
         return;
     }
-    
-    // If this is the "Save As" action on an existing model, or if this is an 
-    // attempt to save a public model that the user doesn't own, then show the 
-    // save dialog box. If this is a save of an existing model that the user 
-    // owns, skip the dialog and save the model to the server. If this is the
-    // "Save" action on a model that is already saved, do nothing.
-    if (isSaveAs) {
-      showSaveDialogBox(Constants.MODELS_SAVEAS_PATH);
-    } else {
-      if (!data.modelIsSaved()) {
 
-        // Don't allow a user to save a model that doesn't belong to them.
-        // Give them the option to save a copy with their username. If they so
-        // choose, be sure to deselect the 'public' label.
-        if (data.getMetadata().getOwner() != data.security.getWmtUsername()) {
-          Boolean saveCopy = Window.confirm(Constants.NOT_MODEL_OWNER);
-          if (saveCopy) {
-            for (Map.Entry<String, LabelJSO> entry : data.modelLabels.entrySet()) {
-              if (entry.getKey().equals("public")) {
-                entry.getValue().isSelected(false);
-              }
-            }
-            showSaveDialogBox(Constants.MODELS_NEW_PATH);
+    // Don't allow a user to save a model that doesn't belong to them. Give
+    // them the option to save a copy with their username. If they so choose,
+    // be sure to deselect the 'public' label.
+    // If this is the "Save As" action on an existing model that the user owns,
+    // then show the save dialog box. If this is a save of an existing model
+    // that the user owns, skip the dialog and save the model to the server. If
+    // this is the "Save" action on a model that is already saved, do nothing.
+    if (!data.getMetadata().getOwner().equals(data.security.getWmtUsername())) {
+      Boolean saveCopy = Window.confirm(Constants.NOT_MODEL_OWNER);
+      if (saveCopy) {
+        for (Map.Entry<String, LabelJSO> entry : data.modelLabels.entrySet()) {
+          if (entry.getKey().equals("public")) {
+            entry.getValue().isSelected(false);
           }
-        } else {
+        }
+        showSaveDialogBox(Constants.MODELS_NEW_PATH);
+      }
+    } else {
+      if (isSaveAs) {
+        showSaveDialogBox(Constants.MODELS_SAVEAS_PATH);
+      } else {
+        if (!data.modelIsSaved()) {
           data.serialize();
           DataTransfer.postModel(data, Constants.MODELS_EDIT_PATH);
         }
-
       }
     }
   }
