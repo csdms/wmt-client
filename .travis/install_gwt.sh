79ebe471c6c167295d1cdc9325a2244eacf31d25
Mark Piper <mark.piper@colorado.edu>
2015-10-12 13:12:20 -0600

    Add debugging information

--- .travis/install_gwt.sh
+++ .travis/install_gwt.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/env bash
 
 GWT_VERSION=2.5.1
-if [ ! -f $HOME/gwt-$GWT_VERSION/gwt-servlet.jar ]; then
+if [ ! -d $HOME/gwt-$GWT_VERSION ]; then
+    rm -rf $HOME/gwt-$GWT_VERSION
     echo "Downloading GWT library."
-    cd $HOME
-    wget http://google-web-toolkit.googlecode.com/files/gwt-$GWT_VERSION.zip
-    unzip -qq gwt-$GWT_VERSION.zip
+    wget http://google-web-toolkit.googlecode.com/files/gwt-$GWT_VERSION.zip \
+	-O $HOME/gwt-$GWT_VERSION.zip
+    unzip -qq gwt-$GWT_VERSION.zip -d $HOME/gwt-$GWT_VERSION
 else
     echo "Using cached GWT directory."
 fi
