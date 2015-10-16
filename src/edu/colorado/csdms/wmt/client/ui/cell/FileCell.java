0fb672858cfb6c61c187f9ce60d8bd98eeeedd2a
Mark Piper <mark.piper@colorado.edu>
2015-10-16 13:25:59 -0600

    Fix issue with uploaded file not appearing in ParameterTable
    This issue was introduced by commit 1c8ed6b6dfe4f621bc878147f1703ea0a41c2579.
    I've modified the change I enacted in that commit.

--- src/edu/colorado/csdms/wmt/client/ui/cell/FileCell.java
+++ src/edu/colorado/csdms/wmt/client/ui/cell/FileCell.java
@@ -67,6 +67,9 @@ public class FileCell extends HorizontalPanel {
     if (selectedIndex > 0) {
       fileDroplist.setSelectedIndex(selectedIndex);
     } else {
+      if (nFiles > 1) {
+        fileDroplist.addItem(parameter.getValue().getDefault());
+      }
       fileDroplist.setSelectedIndex(fileDroplist.getItemCount() - 1);
     }
     fileDroplist.setVisibleItemCount(1);  // show one item -- a droplist
