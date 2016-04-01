978c9055237583c1da7b87dfa46883cd3d9713d9
Mark Piper <mark.piper@colorado.edu>
2016-04-01 17:50:21 -0600

    Move FA icon strings to Constants

--- src/edu/colorado/csdms/wmt/client/ui/panel/ParameterActionPanel.java
+++ src/edu/colorado/csdms/wmt/client/ui/panel/ParameterActionPanel.java
@@ -1,26 +1,3 @@
-/**
- * The MIT License (MIT)
- * 
- * Copyright (c) 2014 mcflugen
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 package edu.colorado.csdms.wmt.client.ui.panel;
 
 import java.util.Iterator;
@@ -62,15 +39,14 @@ public class ParameterActionPanel extends HorizontalPanel {
     this.setStyleName("wmt-ActionPanel");
 
     // Reset
-    Button resetButton = new Button("<i class='fa fa-bolt'></i>");
+    Button resetButton = new Button(Constants.FA_BOLT);
     resetButton.setTitle(Constants.PARAMETER_RESET);
     resetButton.addClickHandler(new ParameterActionPanelResetHandler(this.data,
         this.componentId));
     this.add(resetButton);
 
     // View input files
-    final Button viewFilesButton =
-        new Button("<i class='fa fa-external-link'></i>");
+    final Button viewFilesButton = new Button(Constants.FA_LINK);
     viewFilesButton.setTitle(Constants.PARAMETER_VIEW_FILE);
     this.add(viewFilesButton);
     inputFilesMenu = new ViewInputFilesMenu(this.data, this.componentId);
