17201be5988782240d7a05b1554986112744ace4
Mark Piper <mark.piper@colorado.edu>
2016-04-05 13:30:33 -0600

    Show id of saved model in model open dialog

--- src/edu/colorado/csdms/wmt/client/ui/panel/MetadataPanel.java
+++ src/edu/colorado/csdms/wmt/client/ui/panel/MetadataPanel.java
@@ -4,8 +4,10 @@ import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 
+import edu.colorado.csdms.wmt.client.Constants;
+
 /**
- * Displays the owner and creation date of a model.
+ * Displays the owner, creation date, and internal id of a model.
  * 
  * @author Mark Piper (mark.piper@colorado.edu)
  */
@@ -14,6 +16,7 @@ public class MetadataPanel extends HorizontalPanel {
   private Grid grid;
   private String owner = "";
   private String date = "";
+  private Integer id = Constants.DEFAULT_MODEL_ID;
   
   public MetadataPanel() {
 
@@ -21,14 +24,18 @@ public class MetadataPanel extends HorizontalPanel {
     this.setSpacing(5); // px
     this.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
     
-    grid = new Grid(2, 2);
+    grid = new Grid(3, 2);
     grid.setHTML(0, 0, "owner:");
     grid.setHTML(1, 0, "creation date:");
+    grid.setHTML(2, 0, "id:");
     grid.setHTML(0, 1, owner);
     grid.setHTML(1, 1, date);
+    grid.setHTML(2, 1, id.toString());
     grid.getCellFormatter().setHorizontalAlignment(0, 0,
         HasHorizontalAlignment.ALIGN_RIGHT);
     grid.getCellFormatter().setHorizontalAlignment(1, 0,
+        HasHorizontalAlignment.ALIGN_RIGHT);
+    grid.getCellFormatter().setHorizontalAlignment(2, 0,
         HasHorizontalAlignment.ALIGN_RIGHT);
     
     this.add(grid);
@@ -48,5 +55,13 @@ public class MetadataPanel extends HorizontalPanel {
 
   public void setDate(String date) {
     grid.setHTML(1, 1, date);
+  }
+
+  public String getId() {
+    return grid.getText(2, 1);
+  }
+
+  public void setId(Integer id) {
+    grid.setHTML(2, 1, id.toString());
   }
 }
